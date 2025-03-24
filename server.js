1a13ad806b1da55e58a11d7cf1314ad24b0139d4
Rahul Suthar <154016388+Rahul-Suthar@users.noreply.github.com>
2025-03-24 21:31:24 +0530

    Added Docker Setup

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.set("viwes", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err));
 
